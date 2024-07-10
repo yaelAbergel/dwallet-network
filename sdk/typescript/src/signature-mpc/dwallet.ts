14ff15d43e10d0ae0280c4468aeff185d50d201c
Itay Levy <itaylevy134@gmail.com>
2024-07-10 18:43:23 +0300

    --wip-- [skip ci]

--- sdk/typescript/src/signature-mpc/dwallet.ts
+++ sdk/typescript/src/signature-mpc/dwallet.ts
@@ -93,7 +93,7 @@ export const transferDwallet = async (client: DWalletClient, keypair: Keypair) =
 	const tx = new TransactionBlock();
 	tx.moveCall({
 		target: `${packageId}::dwallet_transfer::transfer_dwallet`,
-		arguments: [],
+		arguments: [tx.object('0xcf577e021171d9b032890d77444df330d01794d702c479df39ca72a26ba78988')],
 		// tx.pure(bcs.vector(bcs.vector(bcs.u8())).serialize(messages))
 	});
 	await client.signAndExecuteTransactionBlock({
